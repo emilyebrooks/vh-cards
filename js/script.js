bd4716e653c2aecb0696e9f42363aea28d912192
em_wilson <emilyellenwilson@gmail.com>
2022-03-08 19:17:05 -0500

    Added some styling and played around with click eventListener changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Create an array of descriptor words
-const descriptor = ["content", "joyful", "anxious", "discouraged", "expectant", "tired", "strong", "capable", "confident", "scared", "worried", "displaced", "secure"];
+const descriptors = ["content", "joyful", "anxious", "discouraged", "expectant", "tired", "strong", "capable", "confident", "scared", "worried", "displaced", "secure"];
 
 //Grab button element
 const btn = document.querySelector(".btn"); 
@@ -9,10 +9,12 @@ const btn = document.querySelector(".btn");
 btn.addEventListener("click", function() {
     const randomNumber = getRandomNumber(); 
     // console.log(randomNumber); 
-    btn.innerText=descriptor[randomNumber];
+    btn.innerText=descriptors[randomNumber]; //changes to a descriptor word when button is clicked
+    btn.style.color= "#8b7b6e"; //changes text color when button is clicked
+    btn.style.fontWeight = "bold"; //changes text to bold when button is clicked
 })
 
 //Run function to generate whole random number within the length of the descriptor array
 function getRandomNumber(){
-    return Math.floor(Math.random() * descriptor.length); 
+    return Math.floor(Math.random() * descriptors.length); 
 }
